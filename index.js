3dfd70a718a5c3984a256de980e6310c7732c057
Victoria <victoria.calvo1013@hotmail.com>
2025-01-18 20:37:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el");
+let count = 0
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+
+}
+
